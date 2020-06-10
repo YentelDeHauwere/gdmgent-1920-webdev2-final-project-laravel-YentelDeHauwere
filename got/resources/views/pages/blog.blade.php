ffb91f2cefabda68427de75fae285d37a159a1df
YentelDeHauwere <yentel.dh@hotmail.be>
2020-06-10 19:43:58 +0200

    Documents

--- got/resources/views/pages/blog.blade.php
+++ got/resources/views/pages/blog.blade.php
@@ -2,13 +2,15 @@
 
 @section('content')
 
-<div class="container-fluid">
+<div class="container-fluid" style="text-align:center;">
 	<img class="blog-right-image" src="{{$blog->image}}" alt="blog">
-	<small class="blog-left-author">Posted By Henry</small>
 	<h6 class="blog-left-title"> {{$blog->title}} </h6>
 	<p class="blog-left-intro"> {{$blog->intro}} </p>
 	<p class="blog-left-intro"> {{$blog->content}} </p>
-						
+	<a class="goback"  href="{{ route('blogs') }}">
+		<i class="fa fa-chevron-left" aria-hidden="true"></i>
+		Go Back 
+	</a>				
 </div>
 
 @endsection
